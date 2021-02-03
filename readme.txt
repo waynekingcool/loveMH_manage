e6fcb5668a616a33cde0a56e4fcd46d52c8882bf
万斌 <2641415119@qq.com>
2021-02-03 10:14:29 +0800

    first

---
+++ readme.txt
@@ -0,0 +1,9 @@
+初始化
+1. vue ui 创建项目
+2.安装element-ui axios node-sass sass sass-loader库
+3.在main.js中引入element axios库.
+4.创建/assets/css,添加glbal.css,并且在main.js中引入.
+5.删除views文件下面不需要的视图,清理components里面默认的组件,清理App.vue文件中的代码.
+6.创建components/login/Login.vue文件,并且在router/index.js中引入,并设置好路由.
+
+
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// element UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// axios
+import axios from 'axios'
+// 全局css
+import './assets/css/global.css'
+
+
+Vue.prototype.$http = axios
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
