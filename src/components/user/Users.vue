2d5af76b9a3bb37b1de1d4c7f4c9a312acbd398d
万斌 <2641415119@qq.com>
2021-02-04 16:38:58 +0800

    添加用户

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -8,24 +8,56 @@
 
         <el-card>
             <!-- 添加用户,相当于注册 -->
-            <el-row>
+            <el-row :gutter="20">
                 <el-col :span="8">
-                    11111
+                    <el-input placeholder="请输入搜索的用户信息">
+                        <el-button slot="append" icon="el-icon-search" @click="searchUser"></el-button>
+                    </el-input>
                 </el-col>
-                <el-col :span="4">222</el-col>
+                <el-col :span="4">
+                    <el-button type="primary" @click="dialogVisible = true">添加用户</el-button>
+                </el-col>
             </el-row>
         </el-card>
 
+        <!-- 添加用户对话框 -->
+        <el-dialog title="添加用户" :visible.sync="dialogVisible">
+            <el-form ref="userFormRef" :model="userModel" label-width="80px" :rules="addUserRules">
+                <el-form-item label="用户名称" prop="userName">
+                    <el-input v-model="userModel.userName"></el-input>
+                </el-form-item>
+                <el-form-item label="用户密码" prop="password">
+                    <el-input v-model="userModel.password"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱" prop="email">
+                    <el-input v-model="userModel.email"></el-input>
+                </el-form-item>
+                <el-form-item label="头像" prop="avator">
+                    <el-upload :action="uploadURL"
+                    :on-preview="handlePreview"
+                    :on-remove="handleRemove"
+                    :on-success="handleSuccess"
+                    :on-error="handleError"
+                    :headers="headerObj"
+                    list-type="picture">
+                        <el-button size="small" type="primary">点击上传</el-button>
+                    </el-upload>
+                </el-form-item>
+                <el-form-item label="管理员" prop="isAdmin">
+                    <el-switch v-model="userModel.isAdmin"
+                    active-color="#409eff"
+                    @change="switchChanged"
+                    >
+                    </el-switch>
+                </el-form-item>
+                <el-form-item class="btns">
+                    <el-button>取消</el-button>
+                    <el-button type="primary" @click="addUser">创建</el-button>
+                </el-form-item>
+            </el-form>
+        </el-dialog>
 
-        <!-- <el-upload :action="uploadURL"
-        :on-preview="handlePreview"
-        :on-remove="handleRemove"
-        :on-success="handleSuccess"
-        :on-error="handleError"
-        :headers="headerObj"
-        list-type="picture">
-            <el-button size="small" type="primary">点击上传</el-button>
-        </el-upload> -->
+
     </div>
 </template>
 
@@ -34,9 +66,35 @@ export default {
     name: 'Users',
     data () {
         return {
+            // 两个上传相关的参数
             uploadURL: 'http://127.0.0.1:3000/api/utils/upload',
             headerObj: {
                 Authorization: window.sessionStorage.getItem('token')
+            },
+            // 对话框显示状态
+            dialogVisible: false,
+            userModel: {
+                userName: '',
+                password: '',
+                email: '',
+                avator: '',
+                isAdmin: false
+            },
+            // 表单验证
+            addUserRules: {
+                userName: [
+                    { required: true, message: "请输入用户名", trigger: 'blur' },
+                    { min: 3, max: 10, message: "长度为3-10个字符", trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: "请输入密码", trigger: 'blur' },
+                    { min: 3, max: 10, message: "长度为3-10个字符", trigger: 'blur' }
+                ],
+                email: [
+                    { required: true, message: "请输入邮箱", trigger: 'blur' },
+                    { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change'}
+                ]
+
             }
         }
     },
@@ -54,6 +112,7 @@ export default {
             if (response.errno == 0) {
                 // 上传成功 图片路径
                 const imgUrl = response.data.url
+                this.userModel.avator = imgUrl
             }
         },
         handleError(err) {
@@ -61,6 +120,30 @@ export default {
                 message: '图片上传失败' + err,
                 type: 'error'
             })
+        },
+        // 搜索用户
+        searchUser() {
+
+        },
+        // 添加用户
+        addUser() {
+            this.$refs.userFormRef.validate( async valid => {
+                if(valid) {
+                    console.log('验证通过');
+                    const { data: res } = await this.$http.post('/api/user/register', this.userModel)
+                    
+                    if (res.errno == 0) {
+                        this.$message.success('添加成功')
+                        this.dialogVisible = false
+                    } else {
+                        this.$message.error('添加失败:' + res.message)
+                    }
+                } 
+            })
+        },
+        // switch切换状态
+        switchChanged(value) {
+            this.userModel.isAdmin = value
         }
     }
 }
@@ -68,5 +151,12 @@ export default {
 
 <style lang="scss" scoped>
 @import '@css/style.scss';
-    
+    .el-card {
+        margin-top: 20px;
+    }
+
+    .btns {
+        display: flex;
+        justify-content: flex-end;
+    }
 </style>
